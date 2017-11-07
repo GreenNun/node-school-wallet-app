da3cbb72e9fe24b87e9204075c462ae73d5c2ee3
GreenNun <stas909@gmail.com>
2017-11-07 18:40:33 +0300

    schedule fix

--- source/controllers/cards/create-dependent.js
+++ source/controllers/cards/create-dependent.js
@@ -1,6 +1,8 @@
 'use strict';
+const AutoPaymentModel = require('../../models/auto-payments');
 
 module.exports = async (ctx) => {
+	const cardId = ctx.params.id;
     const user = await ctx.usersModel.getBy({"login": ctx.request.body.login});
     const userId = user.id;
     const card = {
@@ -10,11 +12,19 @@ module.exports = async (ctx) => {
         parentId: Number(ctx.params.id)
     };
     const newCard = await ctx.cardsModel.create(card);
+
     /**
      * Тут ставится автоплатеж
      */
-
-
+	await new AutoPaymentModel().create({
+		cardId: cardId,
+		sum: parseInt(card.balance, 10),
+		date: new Date(Date.now()),
+		receiverType: 'cardPayment',
+		receiverNumber: card.cardNumber,
+		isDone: false,
+		dateRepeat: 'monthly'
+	});
 
     ctx.status = 201;
     ctx.body = newCard;
